c947fd9bf29e77deea2583b3d178f32a138b79ff
Johnnie <johnnie.hard@gmail.com>
2017-05-10 09:43:13 +0200

    Remove print debugging of api-key

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 var express = require('express');  
 var darkskyDateRange = require("./darksky-daterange.js");
 
-console.log(process.env.DARKSKY_API_KEY);
-
 var app = express();
 app.listen(process.env.PORT || 3000);
 
